30f89c5cc47c82a2dcbd66b0a3580309c8c7d6ac
julianac12 <137023828+julianac12@users.noreply.github.com>
2023-10-19 23:58:06 -0500

    merged Gabbi and I's script
    Fixed some error messages and got all files imported to tables.

--- SQL/project_3_SQL .sql
+++ SQL/project_3_SQL .sql
@@ -1,43 +1,45 @@
 drop table Suppression_Costs_Federal_Firefighting_Costs  cascade;
---drop table Sea_level_grouped_by_years cascade;
---drop table USA_census_population_estimate cascade;
---drop table Clean_climate cascade;
---drop table Merged_fire_archives;
+drop table Sea_level_grouped_by_years cascade;
+drop table USA_census_population_estimate cascade;
+drop table Clean_climate2 cascade;
+drop table HumanCausedFires cascade;
 
 
 create table Suppression_Costs_Federal_Firefighting_Costs (
-	year Date,
-	Fires int,
-	Acres int,
-	"Forest Service ($)" int,
-	"DOI Agencies ($)" int,
-	"Total ($)" int
+	"Year" int,
+	"Fires" int,
+	"Acres" int,
+	"Forest Service ($)" int8,
+	"DOI Agencies ($)" int8,
+	"Total ($)" int8,
+	Constraint "pk_HumanCausedFires" primary key ("Year")
 );
 
 	
 create table Sea_level_grouped_by_years(
-	Year varchar,
+	"Year" int,
 	Measure varchar,
-	"Change in MSL Value" int
+	"Change in MSL Value" int,
+	Constraint "pk_HumanCausedFires" primary key ("Year")
 );
 
 create table USA_census_population_estimate (
-	"Geographical Area" varchar,
-	"2013" int,
-	"2014" int,
-	"2015" int,
-	"2016" int,
-	"2017" int,
-	"2018" int,
-	"2019" int,
-	"2020" int,
-	"2021" int,
-	"2022" int
+	"Geographic Area" varchar,
+	"2013" bigint,
+	"2014" bigint,
+	"2015" bigint,
+	"2016" bigint,
+	"2017" bigint,
+	"2018" bigint,
+	"2019" bigint,
+	"2020" bigint,
+	"2021" bigint,
+	"2022" bigint
 );
 
 create table Clean_climate( 
 	Country varchar not null,
-	Indicator varchar not null,
+	"Indicator" varchar not null,
 	Unit varchar not null,
 	F2012 varchar,
 	F2013 varchar,	
@@ -67,4 +69,72 @@ create table Merged_fire_archives (
 	bright_t31 int,
 	frp int,
 	daynight varchar
+);
+
+create table HumanCausedFires  (
+"Year" int,
+"Alaska" int,
+"Northwest" int,
+"Northern California" int,
+"Southern California" int,
+"Northern Rockies" int,
+"Great Basin" int,
+"Western Great Basin" int,
+"Southwest" int,
+"Rocky Mount" int,
+"Eastern Area" int,
+"Southern Area" int,
+"Total" int,
+constraint "pk_HumanCausedFires" primary key ("Year")
+);
+
+create table HumanAffectedAcreage (
+"Year" int,
+"Alaska" int,
+"Northwest" int,
+"Northern California" int,
+"Southern California" int,
+"Northern Rockies" int,
+"Great Basin" int,
+"Western Great Basin" int,
+"Southwest" int,
+"Rocky Mount" int,
+"Eastern Area" int,
+"Southern Area" int,
+"Total" int,
+Constraint "pk_HumanAffectedAcreage" primary key ("Year")
+);
+
+create table LightningCausedFires (
+"Year" int,
+"Alaska" int,
+"Northwest" int,
+"Northern California" int,
+"Southern California" int,
+"Northern Rockies" int,
+"Great Basin" int,
+"Western Great Basin" int,
+"Southwest" int,
+"Rocky Mount" int,
+"Eastern Area" int,
+"Southern Area" int,
+"Total" int,
+Constraint "pk_LightningCausedFires" primary key ("Year")
+);
+
+create table LightningAffectedAcreage (
+"Year" int,
+"Alaska" int,
+"Northwest" int,
+"Northern California" int,
+"Southern California" int,
+"Northern Rockies" int,
+"Great Basin" int,
+"Western Great Basin" int,
+"Southwest" int,
+"Rocky Mount" int,
+"Eastern Area" int,
+"Southern Area" int,
+"Total" int,
+Constraint "pk_LightningAffectedAcreage" primary key ("Year")
 );
